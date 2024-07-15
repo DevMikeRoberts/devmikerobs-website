9e8b95a59b11fa64dbc4b06a32f88870d7617483
Michael Roberts <mikerobs238@hotmail.com>
2024-07-15 00:55:00 -0400

    reactivity

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 const scrollers = document.querySelectorAll(".scroller");
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop -150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset &&top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.quesrySelector('header nav a[href*=' + id + ']').classList.add('active');
+            })
+        }
+    })
+}
 
 if (!window.matchMedia("(prefers-reduced-motion:reduce)").matches){
     addAnimation();
@@ -17,4 +38,9 @@ function addAnimation() {
             scrollerInner.appendChild(duplicatedItem);
         })
     })
+}
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
 }
